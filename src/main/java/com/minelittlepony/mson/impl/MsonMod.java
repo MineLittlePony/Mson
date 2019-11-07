e2e89f5aafa2a40f25d5e20c72e3b71c66675dc7
Sollace <sollacea@gmail.com>
2019-11-07 23:44:59 +0200

    Make the entrypoint public

--- src/main/java/com/minelittlepony/mson/impl/MsonMod.java
+++ src/main/java/com/minelittlepony/mson/impl/MsonMod.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.minecraft.resource.ResourceType;
 
-class MsonMod implements ClientModInitializer {
+public class MsonMod implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(MsonImpl.INSTANCE);
