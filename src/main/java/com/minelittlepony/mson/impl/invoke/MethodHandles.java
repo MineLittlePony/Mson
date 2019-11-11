0fb35ff89a0c8b0e148cbbc628f6ba5c026807b6
Sollace <sollacea@gmail.com>
2019-11-11 18:34:46 +0200

    Log it properly

--- src/main/java/com/minelittlepony/mson/impl/invoke/MethodHandles.java
+++ src/main/java/com/minelittlepony/mson/impl/invoke/MethodHandles.java
@@ -1,5 +1,7 @@
 package com.minelittlepony.mson.impl.invoke;
 
+import com.minelittlepony.mson.impl.MsonImpl;
+
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodType;
 import java.lang.invoke.MethodHandles.Lookup;
@@ -17,7 +19,7 @@ final class MethodHandles {
             IMPL_LOOKUP.setAccessible(true);
             return (Lookup)IMPL_LOOKUP.get(null);
         } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
-            e.printStackTrace();
+            MsonImpl.LOGGER.error("Could not obtain elevated lookup privileges. All lookups will be performed as a filthy casual.", e);
             return java.lang.invoke.MethodHandles.lookup();
         }
     }
@@ -38,12 +40,12 @@ final class MethodHandles {
                     MethodHandle bound = bindAgumentL.bindTo(handle);
                     return (MethodHandle)bound.invoke(0, object);
                 } catch (Throwable e) {
-                    e.printStackTrace();
+                    MsonImpl.LOGGER.error("bindTo operation failed. This should not happen.", e);
                     return handle;
                 }
             };
         } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException e) {
-            e.printStackTrace();
+            MsonImpl.LOGGER.error("Could not obtain elevated bindTo privileges. All binds will follow casting restrictions..", e);
         }
         return MethodHandle::bindTo;
     }
