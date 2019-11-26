f5c5e5db69ecd8913717c30f901fcf186e80f2ed
Sollace <sollacea@gmail.com>
2019-11-26 12:30:33 +0200

    Allow planars to specify more than one plane per face

--- src/main/java/com/minelittlepony/mson/impl/model/JsonPlanar.java
+++ src/main/java/com/minelittlepony/mson/impl/model/JsonPlanar.java
@@ -19,15 +19,18 @@ import com.minelittlepony.mson.impl.exception.FutureAwaitException;
 import com.minelittlepony.mson.util.Incomplete;
 import com.minelittlepony.mson.util.JsonUtil;
 
+import java.util.ArrayList;
 import java.util.EnumMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 public class JsonPlanar extends JsonCuboid {
     public static final Identifier ID = new Identifier("mson", "planar");
 
-    private final Map<Face, JsonFace> faces = new EnumMap<>(Face.class);
+    private final Map<Face, JsonFaceSet> faces = new EnumMap<>(Face.class);
 
     public JsonPlanar(JsonContext context, JsonObject json) {
         super(context, json);
@@ -35,7 +38,7 @@ public class JsonPlanar extends JsonCuboid {
         Face.VALUES.forEach(face -> {
             JsonUtil.accept(json, face.name().toLowerCase())
                 .map(JsonElement::getAsJsonArray)
-                .ifPresent(el -> faces.put(face, new JsonFace(context, el, face)));
+                .ifPresent(el -> faces.put(face, new JsonFaceSet(context, el, face)));
         });
     }
 
@@ -45,54 +48,76 @@ public class JsonPlanar extends JsonCuboid {
 
         ModelContext subContext = context.resolve(cuboid);
         faces.values().forEach(face -> {
-            try {
-                ((ContentAccessor)cuboid).cubes().add(face.export(subContext));
-            } catch (InterruptedException | ExecutionException e) {
-                throw new FutureAwaitException(e);
-            }
+            face.export(subContext, ((ContentAccessor)cuboid).cubes());
         });
     }
 
-    class JsonFace implements JsonComponent<Cuboid> {
+    class JsonFaceSet {
 
-        private final Face face;
+        private final List<JsonFace> elements = new ArrayList<>();
 
-        private final float[] position = new float[3];
-        private final int[] size = new int[2];
+        public JsonFaceSet(JsonContext context, JsonArray json, Face face) {
 
-        private final Incomplete<Optional<Texture>> texture;
-
-        public JsonFace(JsonContext context, JsonArray json, Face face) {
-            this.face = face;
-
-            position[0] =  json.get(0).getAsFloat();
-            position[1] =  json.get(1).getAsFloat();
-            position[2] =  json.get(2).getAsFloat();
-            size[0] = (int)json.get(3).getAsFloat();
-            size[1] = (int)json.get(4).getAsFloat();
-
-            if (json.size() > 6) {
-                texture = createTexture(json.get(5).getAsInt(), json.get(6).getAsInt());
+            if (json.get(0).isJsonArray()) {
+                for (int i = 0; i < json.size(); i++) {
+                    elements.add(new JsonFace(context, json.get(i).getAsJsonArray(), face));
+                }
             } else {
-                texture = Incomplete.completed(Optional.empty());
+                elements.add(new JsonFace(context, json, face));
             }
         }
 
-        private Incomplete<Optional<Texture>> createTexture(int u, int v) {
-            return locals -> {
-                Texture parent = locals.getTexture().get();
-                return Optional.of(new JsonTexture(u, v, parent.getWidth(), parent.getHeight()));
-            };
+        void export(ModelContext subContext, List<Cuboid> cubes) {
+            cubes.addAll(elements.stream().map(face -> {
+                try {
+                    return face.export(subContext);
+                } catch (InterruptedException | ExecutionException e) {
+                    throw new FutureAwaitException(e);
+                }
+            }).collect(Collectors.toList()));
         }
 
-        @Override
-        public Cuboid export(ModelContext context) throws InterruptedException, ExecutionException {
-            return new BoxBuilder(context)
-                .tex(texture.complete(context))
-                .pos(face.transformPosition(position, context))
-                .size(face.getAxis(), size)
-                .build(QuadsBuilder.plane(face));
-        }
+        class JsonFace implements JsonComponent<Cuboid> {
 
+            private final Face face;
+
+            private final float[] position = new float[3];
+            private final int[] size = new int[2];
+
+            private final Incomplete<Optional<Texture>> texture;
+
+            public JsonFace(JsonContext context, JsonArray json, Face face) {
+                this.face = face;
+
+                position[0] =  json.get(0).getAsFloat();
+                position[1] =  json.get(1).getAsFloat();
+                position[2] =  json.get(2).getAsFloat();
+                size[0] = (int)json.get(3).getAsFloat();
+                size[1] = (int)json.get(4).getAsFloat();
+
+                if (json.size() > 6) {
+                    texture = createTexture(json.get(5).getAsInt(), json.get(6).getAsInt());
+                } else {
+                    texture = Incomplete.completed(Optional.empty());
+                }
+            }
+
+            private Incomplete<Optional<Texture>> createTexture(int u, int v) {
+                return locals -> {
+                    Texture parent = locals.getTexture().get();
+                    return Optional.of(new JsonTexture(u, v, parent.getWidth(), parent.getHeight()));
+                };
+            }
+
+            @Override
+            public Cuboid export(ModelContext context) throws InterruptedException, ExecutionException {
+                return new BoxBuilder(context)
+                    .tex(texture.complete(context))
+                    .pos(face.transformPosition(position, context))
+                    .size(face.getAxis(), size)
+                    .build(QuadsBuilder.plane(face));
+            }
+
+        }
     }
 }
