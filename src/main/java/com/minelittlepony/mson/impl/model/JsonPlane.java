9a1e5b018c62712efb3fb429182584f5d20fea1c
Sollace <sollacea@gmail.com>
2019-11-28 11:48:02 +0200

    Added support for specifying texture coordinates directly on a mson:plane

--- src/main/java/com/minelittlepony/mson/impl/model/JsonPlane.java
+++ src/main/java/com/minelittlepony/mson/impl/model/JsonPlane.java
@@ -9,9 +9,11 @@ import com.minelittlepony.mson.api.json.JsonContext;
 import com.minelittlepony.mson.api.model.BoxBuilder;
 import com.minelittlepony.mson.api.model.Face;
 import com.minelittlepony.mson.api.model.QuadsBuilder;
+import com.minelittlepony.mson.api.model.Texture;
 import com.minelittlepony.mson.util.Incomplete;
 import com.minelittlepony.mson.util.JsonUtil;
 
+import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 
 public class JsonPlane implements JsonComponent<Cuboid> {
@@ -21,6 +23,8 @@ public class JsonPlane implements JsonComponent<Cuboid> {
     private final Incomplete<float[]> position;
     private final Incomplete<int[]> size;
 
+    private final Incomplete<Texture> texture;
+
     private final float[] stretch = new float[3];
 
     private final Face face;
@@ -28,6 +32,7 @@ public class JsonPlane implements JsonComponent<Cuboid> {
     public JsonPlane(JsonContext context, JsonObject json) {
         position = context.getVarLookup().getFloats(json, "position", 3);
         size = context.getVarLookup().getInts(json, "size", 3);
+        texture = JsonTexture.localized(JsonUtil.accept(json, "texture"));
         JsonUtil.getFloats(json, "stretch", stretch);
         face = Face.valueOf(JsonUtil.require(json, "face").getAsString().toUpperCase());
     }
@@ -35,6 +40,7 @@ public class JsonPlane implements JsonComponent<Cuboid> {
     @Override
     public Cuboid export(ModelContext context) throws InterruptedException, ExecutionException {
         return new BoxBuilder(context)
+            .tex(Optional.of(texture.complete(context)))
             .pos(face.transformPosition(position.complete(context), context))
             .size(face.getAxis(), size.complete(context))
             .stretch(stretch)
