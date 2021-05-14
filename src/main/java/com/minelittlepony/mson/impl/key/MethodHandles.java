f71caf7c7344ef88561ba9f4c03dcae6f16711e7
Sollace <sollacea@gmail.com>
2021-05-14 23:53:11 +0200

    Fix #5

--- src/main/java/com/minelittlepony/mson/impl/key/MethodHandles.java
+++ src/main/java/com/minelittlepony/mson/impl/key/MethodHandles.java
@@ -1,37 +1,25 @@
 package com.minelittlepony.mson.impl.key;
 
-import com.minelittlepony.mson.impl.MsonImpl;
-
 import java.lang.invoke.CallSite;
 import java.lang.invoke.LambdaMetafactory;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodType;
 import java.lang.invoke.MethodHandles.Lookup;
-import java.lang.reflect.Field;
 
 final class MethodHandles {
-    private static final Lookup LOOKUP = findTrustedLookup();
-
-    private static Lookup findTrustedLookup() {
-        try {
-            Field IMPL_LOOKUP = java.lang.invoke.MethodHandles.Lookup.class.getDeclaredField("IMPL_LOOKUP");
-            IMPL_LOOKUP.setAccessible(true);
-            return (Lookup)IMPL_LOOKUP.get(null);
-        } catch (Exception e) {
-            MsonImpl.LOGGER.error("Could not obtain elevated lookup privileges. All lookups will be performed as a filthy casual.", e);
-            return java.lang.invoke.MethodHandles.lookup();
-        }
-    }
+    private static final Lookup LOOKUP = java.lang.invoke.MethodHandles.lookup();
 
     public static <T> T lookupConstructor(Class<T> ifaceClass, Class<?> owner, Class<?>... parameters) {
         try {
             MethodType constrType = MethodType.methodType(void.class, parameters);
             MethodType callType = constrType.changeReturnType(owner);
 
-            MethodHandle constr = LOOKUP.findConstructor(owner, constrType);
+            // privateLookupIn (since Java9) lets us access private methods and fields as if we were inside the same class.
+            Lookup lookup = java.lang.invoke.MethodHandles.privateLookupIn(owner, LOOKUP);
+            MethodHandle constr = lookup.findConstructor(owner, constrType);
 
             CallSite site = LambdaMetafactory.metafactory(
-                    LOOKUP.in(owner),
+                    lookup,
                     ifaceClass.getMethods()[0].getName(), // name of the call site's method
                     MethodType.methodType(ifaceClass),    // signature of call site's method
                     callType.erase(),                     // signature of the method to implement
