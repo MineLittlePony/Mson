22d65d4fc7ac30402c74dcb24070f9b9316e43f8
Sollace <sollacea@gmail.com>
2025-10-15 20:18:12 +0100

    Return player render types for the mannequin as well!

--- src/main/java/com/minelittlepony/mson/impl/AppliedEntityRendererRegistry.java
+++ src/main/java/com/minelittlepony/mson/impl/AppliedEntityRendererRegistry.java
@@ -78,7 +78,7 @@ public class AppliedEntityRendererRegistry {
 
     @SuppressWarnings("unchecked")
     public <S extends EntityRenderState> Optional<EntityRenderer<?, ?>> getRenderer(S state) {
-        if (state instanceof PlayerEntityRenderState && state.entityType == EntityType.PLAYER) {
+        if (state instanceof PlayerEntityRenderState && (state.entityType == EntityType.PLAYER || state.entityType == EntityType.MANNEQUIN)) {
             if (!customStateRenderers.isEmpty()) {
                 return customStateRenderers.values().stream()
                     .filter(entry -> ((Predicate<EntityRenderState>)entry.getKey()).test(state))
